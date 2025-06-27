3ee2326aaf17a302194a595d554aab863e4816c6
SOHILVAGHELA <sohilvaghela7863@gmail.com>
2025-06-27 22:20:15 +0530

    project setup

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+    <Button className='bg-orange-400 hover:bg-orange-300'>click me</Button>
+    </>
+  )
+}
+
+export default App
